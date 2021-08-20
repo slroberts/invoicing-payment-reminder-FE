69d4208927251399662d73cfec96a942c999a666
Shomari Roberts <shomariroberts@gmail.com>
2021-08-20 00:58:33 -0400

    created starter components

---
+++ src/components/Dashboard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return <div>Will contain Add Client and Generate Invoice</div>;
+};
+
+export default Dashboard;
---
+++ src/components/Landing.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Landing = () => {
+  return <div>Landing screen with login button</div>;
+};
+
+export default Landing;
