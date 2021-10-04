a0a33b3ff8f1ee485614d2b1a6c578f4fb65c7bc
Shomari Roberts <shomariroberts@gmail.com>
2021-10-04 17:04:52 -0400

    using sqlite3 for dev and testing instead of pg

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,21 +1,34 @@
 // Update with your config settings.
 require('dotenv').config();
 
+const { DATABASE_URL } = process.env;
+
 module.exports = {
   development: {
-    client: 'pg',
-    connection: process.env.DB_URL,
+    client: 'sqlite3',
+    connection: {
+      filename: './database/auth.db3',
+    },
+    useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
     },
     seeds: {
       directory: './database/seeds',
     },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys= ON', done);
+      },
+    },
   },
 
   testing: {
-    client: 'pg',
-    connection: process.env.DB_URL,
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3',
+    },
+    useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
     },
@@ -26,7 +39,11 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DB_URL,
+    connection: DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: {
       directory: './database/migrations',
     },
