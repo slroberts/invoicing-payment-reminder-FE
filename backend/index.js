87a5cc156f1785bb0601f4d2cd65b2b0e1e76816
Shomari Roberts <shomariroberts@gmail.com>
2021-10-05 22:08:29 -0400

    updated variable names

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT;
+const { PORT } = process.env;
+
+const port = PORT;
 
 server.listen(port, () => console.log(`\n== API running on port ${port} ==\n`));
