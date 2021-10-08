039d4ab83e3e7334756e1cb531e01bce2c93e216
Shomari Roberts <shomariroberts@gmail.com>
2021-10-08 16:36:37 -0400

    added properties to button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Button = ({ buttonText, onSignInClick, custom }) => {
+const Button = ({ buttonText, handleOnClick, custom, disabled }) => {
   return (
     <button
-      onClick={() => onSignInClick()}
-      className={`mt-3 p-3 w-48 text-white text-center rounded-full border-none transition duration-300 ease-in-out ${custom}`}
+      onClick={handleOnClick}
+      className={`mt-3 p-3 w-48 text-white text-center rounded-full border-none transition duration-300 ease-in-out bg-gray-500 hover:bg-gray-700 ${custom}`}
+      disabled={disabled}
     >
       {buttonText}
     </button>
