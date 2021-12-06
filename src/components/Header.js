0b9cad7013155ae5314d50d16872712f04c98cf0
Shomari Roberts <shomariroberts@gmail.com>
2021-12-06 14:59:54 -0500

    updated Header component with Link and Button elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from './Button';
 
 const Header = () => {
   return (
-    <h1 className='font-sans font-bold uppercase antialiased text-blue-500 py-8'>
-      Invoicing & payment Reminder
-    </h1>
+    <div className='flex justify-between'>
+      <Link to='/'>
+        <h1 className='font-sans font-bold uppercase antialiased text-blue-500 py-8'>
+          Invoicing & payment Reminder
+        </h1>
+      </Link>
+      <div className='mt-8'>
+        <Button buttonText='Log Out' />
+      </div>
+    </div>
   );
 };
 
