7ab8bafa776bae073ff0ddbe311f43e1ae64fe5e
Shomari Roberts <shomariroberts@gmail.com>
2021-12-08 21:01:19 -0500

    created component for line items table

---
+++ src/features/invoice/InvoiceItemsTable.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const InvoiceItemsTable = ({ items, handleDelete }) => {
+  const renderInvoiceItems = items.map((item) => (
+    <tr key={item.id}>
+      <td className='px-6 py-4 whitespace-nowrap'>
+        <div className='text-sm font-medium text-gray-900'>{item.name}</div>
+      </td>
+      <td className='px-6 py-4 whitespace-nowrap'>
+        <div className='text-sm text-gray-900'>$ {item.rate}</div>
+      </td>
+      <td className='px-6 py-4 whitespace-nowrap'>
+        <div className='text-sm text-gray-900'>{item.hours}</div>
+      </td>
+      <td className='px-6 py-4 whitespace-nowrap text-right text-sm font-medium'>
+        <div
+          onClick={handleDelete}
+          className='text-indigo-600 hover:text-indigo-900'
+        >
+          Delete
+        </div>
+      </td>
+    </tr>
+  ));
+
+  return (
+    <table className='min-w-full divide-y divide-gray-200'>
+      <thead className='bg-gray-50'>
+        <tr>
+          <th
+            scope='col'
+            className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+          >
+            Item
+          </th>
+          <th
+            scope='col'
+            className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+          >
+            Rate
+          </th>
+          <th
+            scope='col'
+            className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+          >
+            Hours
+          </th>
+          <th scope='col' className='relative px-6 py-3'>
+            <span className='sr-only'>Delete</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody className='bg-white divide-y divide-gray-200'>
+        {renderInvoiceItems}
+      </tbody>
+    </table>
+  );
+};
+
+export default InvoiceItemsTable;
