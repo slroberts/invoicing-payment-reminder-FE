e6856fd2b741aa0a44334b3da8f2d7d43b758eb8
Shomari Roberts <shomariroberts@gmail.com>
2021-12-09 11:09:07 -0500

    added headings and date

--- src/features/invoice/InvoiceClientInfo.js
+++ src/features/invoice/InvoiceClientInfo.js
@@ -3,12 +3,20 @@ import React from 'react';
 const InvoiceClientInfo = ({ client }) => {
   return (
     <div className='flex-none w:1/5 pr-24 md:text-left mb-10'>
-      <h2 className='font-semibold text-xs uppercase text-gray-400 mb-2'>
+      <h2 className='font-semibold text-sm uppercase text-gray-800 mb-2'>
         Client Information
       </h2>
+
+      <h4 className='font-bold text-xs uppercase mt-2 text-gray-400'>Name</h4>
       <p>{client.name}</p>
+      <h4 className='font-bold text-xs uppercase mt-2 text-gray-400'>Email</h4>
       <p>{client.email}</p>
+      <h4 className='font-bold text-xs uppercase mt-2 text-gray-400'>Phone</h4>
       <p>{client.phone}</p>
+      <h4 className='font-bold text-xs uppercase mt-2 text-gray-400'>
+        Invoice Date
+      </h4>
+      <p className='font-medium'>{new Date().toLocaleDateString()}</p>
     </div>
   );
 };
